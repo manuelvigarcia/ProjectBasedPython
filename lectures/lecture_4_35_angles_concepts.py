7f5f67ae9ef43821dfc107c3a33f3acd10b5971c
Manuel Vicente García Rodríguez <manuelvgr@ext.inditex.com>
2025-04-30 09:44:16 +0200

    Added another way to draw the 8-sector circle for angle concepts lecture.

--- lectures/lecture_4_35_angles_concepts.py
+++ lectures/lecture_4_35_angles_concepts.py
@@ -3,82 +3,53 @@ import turtle as t
 
 root = tk.Tk()
 
-#four ways to draw the angles' circle
+#five ways to draw the angles' circle
 
-#four sectors, then circle
-
+#somewhat pair by pair of sectors
+t.right(90)
 t.forward(200)
-t.left(180)
-
-t.forward(400)
-t.left(90)
-t.circle(200,45)
-t.left(90)
-t.forward(400)
-t.left(90)
-t.circle(200,-90)
 
 t.left(90)
+t.circle(200, -45)
+t.left(90)
 t.forward(400)
+
 t.left(90)
 t.circle(200,-45)
 t.left(90)
 t.forward(400)
-t.left(90)
-t.circle(200)
 
-
-
-
-"""
-
-# by oposite pairs of sectors
-t.forward(200)
-t.left(90)
-t.circle(200,45)
-t.left(90)
-t.forward(400)
 t.left(90)
 t.circle(200,-45)
 t.left(90)
-t.forward(200)
-t.left(45)
-
-t.forward(200)
-t.left(90)
-t.circle(200,45)
-t.left(90)
 t.forward(400)
+
 t.left(90)
 t.circle(200,-45)
 t.left(90)
-t.forward(200)
-t.left(45)
-
-t.forward(200)
-t.left(90)
-t.circle(200,45)
-t.left(90)
 t.forward(400)
+
 t.left(90)
 t.circle(200,-45)
 t.left(90)
-t.forward(200)
-t.left(45)
-
-t.forward(200)
-t.left(90)
-t.circle(200,45)
-t.left(90)
 t.forward(400)
+
 t.left(90)
 t.circle(200,-45)
 t.left(90)
-t.forward(200)
-t.left(45)
+t.forward(400)
 
-"""
-"""
+t.left(90)
+t.circle(200,-45)
+t.left(90)
+t.forward(400)
+
+t.left(90)
+t.circle(200,-45)
+
+input('Press enter to continue . . .')
+t.clearscreen()
+
 #Sector by sector
 t.right(90)
 
@@ -137,49 +108,100 @@ t.circle(200, 45)
 t.left(90)
 t.forward(200)
 t.right(180)
-"""
 
-"""
-#somewhat pair by pair of sectors
-t.right(90)
+
+input('Press enter to continue . . .')
+t.clearscreen()
+
+# by oposite pairs of sectors
 t.forward(200)
-
 t.left(90)
-t.circle(200, -45)
+t.circle(200,45)
 t.left(90)
 t.forward(400)
+t.left(90)
+t.circle(200,-45)
+t.left(90)
+t.forward(200)
+t.left(45)
 
+t.forward(200)
+t.left(90)
+t.circle(200,45)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.circle(200,-45)
+t.left(90)
+t.forward(200)
+t.left(45)
+
+t.forward(200)
+t.left(90)
+t.circle(200,45)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.circle(200,-45)
+t.left(90)
+t.forward(200)
+t.left(45)
+
+t.forward(200)
+t.left(90)
+t.circle(200,45)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.circle(200,-45)
+t.left(90)
+t.forward(200)
+t.left(45)
+
+input('Press enter to continue . . .')
+t.clearscreen()
+
+#four sectors, then circle
+
+t.forward(200)
+t.left(180)
+
+t.forward(400)
+t.left(90)
+t.circle(200,45)
+t.left(90)
+t.forward(400)
+t.left(90)
+t.circle(200,-90)
+
+t.left(90)
+t.forward(400)
 t.left(90)
 t.circle(200,-45)
 t.left(90)
 t.forward(400)
+t.left(90)
+t.circle(200)
 
-t.left(90)
-t.circle(200,-45)
-t.left(90)
-t.forward(400)
+input('Press enter to continue . . .')
+t.clearscreen()
 
-t.left(90)
-t.circle(200,-45)
-t.left(90)
-t.forward(400)
+#draw each petal
+def draw_sectors(radius=100, sectors=4):
+    angles = 360 / sectors
+    for i in range (0,sectors):
+        t.forward(radius)
+        t.left(90)
+        t.circle(radius,angles)
+        t.penup()
+        t.setposition(0,0)
+        t.right(90)
+        t.pendown()
 
-t.left(90)
-t.circle(200,-45)
-t.left(90)
-t.forward(400)
 
-t.left(90)
-t.circle(200,-45)
-t.left(90)
-t.forward(400)
+#draw 8 petals
+sectors = 8
+radius = 200
+draw_sectors(radius, sectors)
 
-t.left(90)
-t.circle(200,-45)
-t.left(90)
-t.forward(400)
-
-t.left(90)
-t.circle(200,-45)
-"""
 root.mainloop()
